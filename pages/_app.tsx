03bd79086b3e46f2367fc83d57cefe3bcc370c53
Ginachiko1998 <soniagina669@gmail.com>
2023-05-26 19:47:30 +0700

    change netlify.toml

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import createSagaMiddleware from 'redux-saga';
-import postReducer from '../postSlice';
-import postSaga from '../postSaga';
-import Home from './index';
+import React, { useEffect } from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import postReducer from "../postSlice";
+import postSaga from "../postSaga";
+import Home from "./index";
 
-import '@/styles/globals.css';
+import "@/styles/globals.css";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -19,7 +19,11 @@ const store = configureStore({
 
 const App: React.FC = () => {
   useEffect(() => {
-    sagaMiddleware.run(postSaga);
+    try {
+      sagaMiddleware.run(postSaga);
+    } catch (error) {
+      console.log({ error });
+    }
   }, []);
 
   return (
