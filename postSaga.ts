74a0135f45bca330c53ca3af81f00d03ce041bc0
Ginachiko1998 <soniagina669@gmail.com>
2023-05-26 19:59:23 +0700

    change netlify.toml

--- postSaga.ts
+++ postSaga.ts
@@ -156,5 +156,5 @@ function* fetchPostsSaga(): Generator<any, void, any> {
 
 export default function* postSaga(): Generator {
   console.log("masuk generator");
-  yield takeLatest(getPostFetch.type, fetchPostsSaga);
+  yield takeLatest(getPostFetch, fetchPostsSaga);
 }
