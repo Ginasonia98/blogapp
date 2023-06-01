e2bb887d222796e6bd58c5c38e512f1c52258b0e
Ginachiko1998 <soniagina669@gmail.com>
2023-06-01 13:06:15 +0700

    add margin top

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ const Home: React.FC = () => {
         Favourite Natural Destinations
       </h2>
       <SearchBar onSearch={handleSearch} />
-      <div className="text-gray-700 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="text-gray-700 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-10">
         {currentPosts &&
           currentPosts.map((post: any, index: number) => (
             <div
